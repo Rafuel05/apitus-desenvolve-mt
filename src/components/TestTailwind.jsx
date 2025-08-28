fa03876c3873cf3a532b9c38b50885c89fc975c0
Rafuel05 <rafael.rmo.123456789@gmail.com>
2025-08-28 10:50:56 -0400

    feat: configurando tailwind

---
+++ src/components/TestTailwind.jsx
@@ -0,0 +1,12 @@
+const TestTailwind = () => {
+  return (
+    <div className="bg-green-500 p-8">
+      <h1 className="text-white text-4xl">Teste Tailwind</h1>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
+        Botão Azul
+      </button>
+    </div>
+  )
+}
+
+export default TestTailwind
